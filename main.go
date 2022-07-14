5129731f0cf4803d710df4030114e653b9777f0d
Ethan Holz <iplaybassx2@gmail.com>
2022-07-13 21:43:56 -0500

    Changed to return faster

--- main.go
+++ main.go
@@ -1,41 +1,40 @@
 package main
 
 import (
-    "net/http"
-    "context"
-    "fmt"
-    "os"
-    "io"
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
 
-    "github.com/docker/docker/api/types"
-    "github.com/docker/docker/client"
-    "github.com/gin-gonic/gin"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"github.com/gin-gonic/gin"
 )
 
 func pullContainer(c *gin.Context) {
-    imageName := "ghcr.io/home-assistant/home-assistant:stable"
-    ctx := context.Background()
-    cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-    if err != nil {
-        panic(err)
-    }
-    fmt.Println("Imported context")
-   // Pull updated image 
-    out, err := cli.ImagePull(ctx, imageName, types.ImagePullOptions{})
-    if err != nil {
-        panic(err)
-    }
-    fmt.Println("Pulled image")
-    defer out.Close()
-    io.Copy(os.Stdout, out)
-    c.JSON(http.StatusCreated, nil)
+	// Accepts and run
+	imageName := "ghcr.io/home-assistant/home-assistant:stable"
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
+	c.JSON(http.StatusAccepted, nil)
+	fmt.Println("Imported context")
+	// Pull updated image
+	out, err := cli.ImagePull(ctx, imageName, types.ImagePullOptions{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Pulled image")
+	defer out.Close()
+	io.Copy(os.Stdout, out)
 }
 
 func main() {
-    router := gin.Default()
-    router.SetTrustedProxies([]string{"127.0.0.1"})
-    router.POST("/update", pullContainer)
-    router.Run("localhost:8081")
+	router := gin.Default()
+	router.SetTrustedProxies([]string{"127.0.0.1"})
+	router.POST("/update", pullContainer)
+	router.Run("localhost:8081")
 }
-
-
