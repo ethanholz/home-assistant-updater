e65c15d93f1f3116ff3638f8eaa55d19c9e0f6dd
Ethan Holz <iplaybassx2@gmail.com>
2022-07-14 18:29:09 -0500

    Added HASSOS notification

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -11,6 +13,33 @@ import (
 	"github.com/docker/docker/client"
 	"github.com/gin-gonic/gin"
 )
+
+// Posts update to Home Assistant Entity
+func postUpdate() {
+	endpoint := "http://localhost:8123/api/services/input_boolean/turn_on"
+	auth_token := os.Getenv("HASS_TOKEN")
+	object := map[string]string{"entity_id": "input_boolean.update"}
+	jsonValue, err := json.Marshal(object)
+	if err != nil {
+		panic(err)
+	}
+
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", endpoint, bytes.NewBuffer(jsonValue))
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer "+auth_token)
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	if resp.Status != "200 OK" {
+		panic("Failed to make request")
+	}
+
+}
 
 func backgroundPull() {
 	imageName := "ghcr.io/home-assistant/home-assistant:stable"
@@ -28,6 +57,7 @@ func backgroundPull() {
 	fmt.Println("Pulled image")
 	defer out.Close()
 	io.Copy(os.Stdout, out)
+	go postUpdate()
 }
 
 func pullContainer(c *gin.Context) {
